b2e5008ffdcfdcf79ead0efe72e5871c515e135e
Richard Levente Hodi <88036014+Sterbike@users.noreply.github.com>
2023-11-09 11:26:41 +0100

    Add files via upload

---
+++ client/client.js
@@ -0,0 +1,43 @@
+const socket = io()
+
+const remc = document.getElementById('e')
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const messages = document.getElementById('messages')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    if (input.value) {
+        socket.emit('msg', input.value)
+        input.value = ''
+    }
+})
+
+function f(e) {
+    socket.emit('cur', { x: e.x, y: e.y })
+    return false
+}
+
+socket.on('msg', msg => {
+    const item = document.createElement('li')
+    if (msg.includes('http://') || msg.includes('https://')) {
+        const link = document.createElement('a')
+        link.setAttribute('href', msg)
+        link.setAttribute('target', '_blank')
+        link.textContent = "LINK"
+        item.appendChild(link)
+    }
+    else {
+        item.textContent = msg
+    }
+    messages.appendChild(item)
+    
+    messages.scrollTop = messages.scrollHeight;
+
+    window.scrollTo(0, document.body.scrollHeight);
+})
+
+socket.on('cur', msg => {
+    remc.style.left = `${msg.x - 6}px`
+    remc.style.top = `${msg.y - 6}px`
+})
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const { createServer } = require('node:http')
+const { Server } = require('socket.io')
+
+const app = express()
+app.use(express.static('client'))
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', s => {
+    s.on('msg', msg => {
+        io.emit('msg', msg)
+    })
+    s.on('cur', msg => {
+        io.emit('cur', msg)
+    })
+})
+
+server.listen(3000, '10.10.10.83', () => {
+    console.log('server running at http://10.10.10.83:3000')
+})
